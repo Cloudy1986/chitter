5ee85b8df528da8c659d41d62f9ac0c959958312
Mark McLeod <markmcleod45@gmail.com>
2022-02-25 13:35:46 +0000

    Use postgreSQL instead of sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_02_25_125636) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "peeps", force: :cascade do |t|
     t.text "message"
     t.datetime "created_at", precision: 6, null: false
