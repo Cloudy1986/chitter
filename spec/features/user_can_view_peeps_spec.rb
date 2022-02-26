012c9a004bf13feae5ff9ae1a727f29bda9bc560
Mark McLeod <markmcleod45@gmail.com>
2022-02-26 11:45:13 +0000

    Feature - display when peep was created

--- spec/features/user_can_view_peeps_spec.rb
+++ spec/features/user_can_view_peeps_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 RSpec.feature 'View peeps', type: :feature do
   scenario 'User can view a list of peeps' do
-    Peep.new(message: 'This is peep 1').save
-    Peep.new(message: 'This is peep 2').save
-    Peep.new(message: 'This is peep 3').save
+    Peep.create(message: 'This is peep 1')
+    Peep.create(message: 'This is peep 2')
+    Peep.create(message: 'This is peep 3')
     visit '/peeps'
     expect(page).to have_content 'This is peep 1'
     expect(page).to have_content 'This is peep 2'
     expect(page).to have_content 'This is peep 3'
+  end
+
+  scenario 'User can view when a peep was created' do
+    peep = Peep.create(message: 'This is peep 1')
+    p peep
+    visit '/peeps'
+    expect(page).to have_content 'This is peep 1'
+    expect(page).to have_content(Time.parse("#{peep.created_at}").strftime("%H:%M %d/%m/%Y"))
   end
 end
