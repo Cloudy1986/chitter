ca12f74cfa67f4284e2357f0524ae3a4521cc192
Mark McLeod <markmcleod45@gmail.com>
2022-02-28 12:20:26 +0000

    Display email of peep owner

--- spec/features/user_can_view_peeps_spec.rb
+++ spec/features/user_can_view_peeps_spec.rb
@@ -19,4 +19,12 @@ RSpec.feature 'View peeps', type: :feature do
     expect(page).to have_content 'This is peep 1'
     expect(page).to have_content(Time.parse("#{peep.created_at}").strftime("%H:%M %d/%m/%Y"))
   end
+
+  scenario 'User can view who created a peep' do
+    user = User.create(email: 'test@example.com', password: '123456')
+    peep = Peep.create(message: 'This is peep 1', user_id: user.id)
+    visit '/peeps'
+    expect(page).to have_content 'This is peep 1'
+    expect(page).to have_content user.email
+  end
 end
