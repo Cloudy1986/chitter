f28d42356e78eaec84a92b54e45282d16bb3a942
Mark McLeod <markmcleod45@gmail.com>
2022-03-01 18:02:29 +0000

    Feature - logged in users can add a comment on a peep

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,19 @@ class CommentsController < ApplicationController
 
   def new
     require_user
+    @peep = Peep.find_by(id: params[:peep_id])
     @comment = Comment.new
   end
+
+  def create
+    @peep = Peep.find(params[:peep_id])
+    @comment = @peep.comments.create(body: comment_params[:body], user_id: current_user.id)
+    redirect_to peep_comments_path
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 
 end
