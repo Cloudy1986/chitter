538a967ebc89f8c2f2bb93fdadc8a2c9181e0054
Mark McLeod <markmcleod45@gmail.com>
2022-03-01 18:17:02 +0000

    Add test for not logged in

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -13,13 +13,19 @@ RSpec.describe CommentsController, type: :controller do
   end
 
   describe 'GET /peeps/:id/comments/new' do
-    it 'responds with status 200' do
+    it 'responds with status 200 if logged in' do
       user = User.create(email: 'test@example.com', password: 'password123')
       peep = Peep.create(message: 'This is a peep', user_id: user.id)
-      # comment = Comment.create(body: 'This is a comment', peep_id: peep.id, user_id: user.id)
       session[:user_id] = user.id
       get :new, params: { peep_id: peep.id }
       expect(response).to have_http_status(200)
+    end
+
+    it 'redirects if not logged in' do
+      user = User.create(email: 'test@example.com', password: 'password123')
+      peep = Peep.create(message: 'This is a peep', user_id: user.id)
+      get :new, params: { peep_id: peep.id }
+      expect(response).to redirect_to '/log-in'
     end
   end
 end
