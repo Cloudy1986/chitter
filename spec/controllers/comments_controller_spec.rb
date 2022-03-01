b7b6fa7565cdc85f4f5b77ef7edfdd76285fb4ee
Mark McLeod <markmcleod45@gmail.com>
2022-03-01 16:01:27 +0000

    Add test for GET /peeps/:id/comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-
+  describe 'GET /peeps/:id/comments' do
+    it 'responds with status 200' do
+      user = User.create(email: 'test@example.com', password: 'password123')
+      peep = Peep.create(message: 'This is a peep', user_id: user.id)
+      comment = Comment.create(body: 'This is a comment', peep_id: peep.id, user_id: user.id)
+      session[:user_id] = user.id
+      get :index, params: { peep_id: peep.id }
+      expect(response).to have_http_status(200)
+    end
+  end
 end
